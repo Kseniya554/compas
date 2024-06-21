6fad8eb52aea37e7306c37b3ea11375e49cef9bb
Kseniya <ksuta_marfa@mil.ru>
2024-06-21 15:37:36 +0300

    Updates

--- _nuxt/Bvwp_Fen.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,v as t,t as o,C as c,D as s,x as _}from"./CEcn_zIe.js";const d={},n=e=>(c("data-v-ae58b1ae"),e=e(),s(),e),p=n(()=>_("h2",null,"Пустая страница",-1)),r=[p];function l(e,i){return o(),t("div",null,r)}const h=a(d,[["render",l],["__scopeId","data-v-ae58b1ae"]]);export{h as default};
